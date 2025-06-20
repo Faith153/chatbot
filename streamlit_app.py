edbb5bf03e8e5c007f38e97c143d653c9234deb3
Faith153 <157795726+Faith153@users.noreply.github.com>
2025-06-20 16:51:52 +0900

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,14 @@ import streamlit as st
 from openai import OpenAI
 
 st.title("뼈 때려주는 멱살 파트너봇")
-st.write(
-    "위트 있는 공격성과 현실 타파 조언을 동시에 제공해, 당신의 아이디어를 멱살 잡고 한 단계 업그레이드시키는 서포터봇"
-    "이 앱을 사용하려면 OpenAI API 키가 필요합니다. [여기](https://platform.openai.com/account/api-keys)에서 API 키를 발급받을 수 있습니다. "
-    "이 앱을 단계별로 직접 만들어보고 싶으시다면 [이 튜토리얼](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)을 참고하세요."
-)
+st.markdown(
+    """
+위트 있는 공격성과 현실 타파 조언을 동시에 제공해,  
+당신의 아이디어를 멱살 잡고 한 단계 업그레이드시키는 서포터봇입니다.  
+이 앱을 사용하려면 OpenAI API 키가 필요합니다. [여기](https://platform.openai.com/account/api-keys)에서 API 키를 발급받을 수 있습니다.  
+이 앱을 단계별로 직접 만들어보고 싶으시다면 [이 튜토리얼](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)을 참고하세요.
+""",
+    unsafe_allow_html=True)
 
 openai_api_key = st.text_input("OpenAI API 키", type="password")
 if not openai_api_key:
